f6e1852606f941b86d1edfbb09e78f703192bbf7
Michael Aldridge <aldridge.mac@gmail.com>
2017-04-01 19:26:22 -0500

    Add regex route for editing an existing poll

--- constellation_vote/urls.py
+++ constellation_vote/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
 
     url(r'^manage/poll$', views.manage_poll.as_view(),
         name="manage_poll"),
+    url(r'^manage/poll/[0-9a-f]{12}4[0-9a-f]{3}[89ab][0-9a-f]{15}\Z$',
+        views.manage_poll.as_view(),
+        name="manage_poll"),
 ]
